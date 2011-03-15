24ab8a9684b3eac36ced0475c212f9a6d8ac4f95
Mathieu Lecarme <mathieu@garambrogne.net>
2011-03-15 23:36:02 +0100

    namespace

--- lib/html2tex.rb
+++ lib/html2tex.rb
@@ -1,59 +1,60 @@
 require 'nokogiri'
 
-@tags = {}
+module Html2tex
+	@tags = {}
 
-#
-def tag name, latex
-	@tags[name.id2name] = LatexTag.new(name, "\\#{latex}{", '}')
-end
-
-#\begin \end
-def begin_end_tag name, latex
-	@tags[name.id2name] = LatexTag.new(name , "\\begin{#{latex}}", "\\end{#{latex}}")
-end
-
-#just a mark
-def empty_tag name, latex
-	@tags[name.id2name] = LatexTag.new(name, "\\{#{latex}}")
-end
-
-class LatexTag
-	attr_reader :start, :stop
-	def initialize name, start=nil, stop=nil
-		@name = name
-		@start = start
-		@stop = stop
+	#
+	def Html2tex.tag name, latex
+		@tags[name.id2name] = LatexTag.new(name, "\\#{latex}{", '}')
 	end
-end
 
-class XHTML2TEX < Nokogiri::XML::SAX::Document
-	attr :buffer
-	def initialize tags
-		@tags = tags
-		@buffer = ""
+	#\begin \end
+	def Html2tex.begin_end_tag name, latex
+		@tags[name.id2name] = LatexTag.new(name , "\\begin{#{latex}}", "\\end{#{latex}}")
 	end
-	def start_element name, attributes = []
-		if @tags.key? name
-			@buffer += @tags[name].start
+
+	#just a mark
+	def Html2tex.empty_tag name, latex
+		@tags[name.id2name] = LatexTag.new(name, "\\{#{latex}}")
+	end
+
+	class LatexTag
+		attr_reader :start, :stop
+		def initialize name, start=nil, stop=nil
+			@name = name
+			@start = start
+			@stop = stop
 		end
 	end
-	def characters string
-		@buffer += string
-	end
-	def end_element name
-		if @tags.key? name
-			@buffer += @tags[name].stop
+
+	class XHTML2TEX < Nokogiri::XML::SAX::Document
+		attr :buffer
+		def initialize tags
+			@tags = tags
+			@buffer = ""
+		end
+		def start_element name, attributes = []
+			if @tags.key? name
+				@buffer += @tags[name].start
+			end
+		end
+		def characters string
+			@buffer += string
+		end
+		def end_element name
+			if @tags.key? name
+				@buffer += @tags[name].stop
+			end
 		end
 	end
-end
 
-def parse html
-	buffer = ""
-	x = XHTML2TEX.new @tags
-	parser = Nokogiri::HTML::SAX::Parser.new x
-	parser.parse html
-	return x.buffer
+	def Html2tex.parse html
+		buffer = ""
+		x = XHTML2TEX.new @tags
+		parser = Nokogiri::HTML::SAX::Parser.new x
+		parser.parse html
+		return x.buffer
+	end
 end
-
 #parser = Nokogiri::HTML::SAX::Parser.new(XHTML2TEX.new)
 #parser.parse(File.read(ARGV[0], 'rb'))
--- rakelib/latex.rake
+++ rakelib/latex.rake
@@ -1,16 +1,16 @@
 require 'nokogiri'
 require 'lib/html2tex'
 
-tag :h1, 'section'
-tag :h2, 'subsection'
+Html2tex.tag :h1, 'section'
+Html2tex.tag :h2, 'subsection'
 
-begin_end_tag :code, 'verbatim'
-begin_end_tag :ul,   'enumerate'
+Html2tex.begin_end_tag :code, 'verbatim'
+Html2tex.begin_end_tag :ul,   'enumerate'
 
-empty_tag :li, 'item'
+Html2tex.empty_tag :li, 'item'
 
-tag :em,     'textit'
-tag :strong, 'textbf'
+Html2tex.tag :em,     'textit'
+Html2tex.tag :strong, 'textbf'
 
 namespace :latex do
 	desc "html vers latex"
@@ -19,7 +19,7 @@ namespace :latex do
 		f = File.open(t.source)
 		#doc = Nokogiri::HTML(f)
 		#p doc.root()
-		puts parse(f.read)
+		puts Html2tex.parse(f.read)
 		f.close
 		# out = File.open(t.name, 'w')
 		# out.write 'prout'
