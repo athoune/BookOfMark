eb12639f9a7917a7c8336bd358e6f69c4de59dfa
Mathieu Lecarme <mathieu@garambrogne.net>
2011-03-19 23:37:58 +0100

    accessor

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'yaml'
+require 'rake'
 require 'maruku'
 
 require 'html2index'
@@ -13,14 +14,23 @@ module BookOfMark
 			@path = description
 			folder = description.split('/')
 			if folder.length == 1
-				@folder = nil
+				@folder = '.'
 			else
 				@folder = folder[0..-2].join('/') + '/'
 			end
 			@config = YAML.load_file description
 		end
-		def source
+		def toc
 			@config['toc']
+		end
+		def title
+			@config['title']
+		end
+		def markdown_files
+			FileList.new self.toc.map{ |source| "#{@folder}/source/#{source}"}
+		end
+		def html_files
+			FileList.new self.toc.map{ |source| "#{@folder}/build/raw_html/#{source.ext('html')}"}
 		end
 		#concatened markdown source
 		def sources
